1ea54e509ae467dc3b6020c486954f19b1482ec4
DouwdSlash <guillaumelamarque7@gmail.com>
2025-02-26 17:14:22 +0100

    change fonts

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,6 +5,10 @@ function Header() {
     return (
         <header>
             <img src={logo} alt="test" />
+            <div>
+                <h1>On commande quoi</h1>
+                <h2>Aujourd'hui?</h2>
+            </div>
         </header>
     );
     }
